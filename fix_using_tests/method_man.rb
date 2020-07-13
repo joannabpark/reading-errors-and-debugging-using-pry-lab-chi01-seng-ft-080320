8fc2e9b005ba6f2075797e12915cf4e114c9beff
JoAnna Park <joannabpark1@gmail.com>
2020-07-13 22:40:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,7 @@
 # don't forget to add: require 'pry'
-player1 = "Rocky"
-player2 = "Bullwinkle"
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
