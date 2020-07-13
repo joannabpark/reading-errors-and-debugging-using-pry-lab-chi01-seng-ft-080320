8d8338de89abf3f7be4a0a5cd726ac016d068cc3
JoAnna Park <joannabpark1@gmail.com>
2020-07-13 22:46:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[10] == "s"
-    10 * "s" + string
+  if string[0] == "s"
+    10.to.i * "s" + string
   else
     string
   end
